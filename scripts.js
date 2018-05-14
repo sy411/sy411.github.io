ea9cee841564669a5a2e0c6b9a0736877ce8a755
Sy Harger <silasharger@gmail.com>
2018-05-14 16:55:15 +0200

    button with quiz working

---
+++ scripts.js
@@ -0,0 +1,43 @@
+// let name = "Silas Harger";
+// while (name !== "Silas Harger") {
+//   console.log(`The name is ${name}`);
+//   name = prompt("What is my name?");
+//   if (name !== "Silas Harger") {
+//     alert("Try again!");
+//   } else {
+//     alert("Correct!");
+//   }
+// }
+
+// let answer = "11";
+// while (answer !== "11") {
+//   answer = prompt("How old am I?");
+//   if (answer !== "11") {
+//     alert("Try again?");
+//   } else {
+//     alert("Correct!");
+//   }
+// }
+
+// let born = "Atlanta, Georgia";
+// while (born !== "Atlanta, Georgia") {
+//   born = prompt("Where was I born?");
+//   if (born !== "Atlanta, Georgia") {
+//     alert("Try again!");
+//   } else {
+//     alert("Correct!");
+//   }
+// }
+
+function takeNameQuiz() {
+  let name = "";
+  while (name !== "Silas Harger") {
+    console.log(`The name is ${name}`);
+    name = prompt("What is my name?");
+    if (name !== "Silas Harger") {
+      alert("Try again!");
+    } else {
+      alert("Correct!");
+    }
+  }
+}
