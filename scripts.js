d0372e4080393a11e2ea38bda01eb11a816dec82
Sy Harger <silasharger@gmail.com>
2018-05-14 17:43:35 +0200

    case insensitive

--- scripts.js
+++ scripts.js
@@ -1,43 +1,47 @@
-// let name = "Silas Harger";
-// while (name !== "Silas Harger") {
-//   console.log(`The name is ${name}`);
-//   name = prompt("What is my name?");
-//   if (name !== "Silas Harger") {
-//     alert("Try again!");
-//   } else {
-//     alert("Correct!");
-//   }
-// }
-
-// let answer = "11";
-// while (answer !== "11") {
-//   answer = prompt("How old am I?");
-//   if (answer !== "11") {
-//     alert("Try again?");
-//   } else {
-//     alert("Correct!");
-//   }
-// }
-
-// let born = "Atlanta, Georgia";
-// while (born !== "Atlanta, Georgia") {
-//   born = prompt("Where was I born?");
-//   if (born !== "Atlanta, Georgia") {
-//     alert("Try again!");
-//   } else {
-//     alert("Correct!");
-//   }
-// }
-
-function takeNameQuiz() {
+function takeQuiz() {
   let name = "";
-  while (name !== "Silas Harger") {
+  while (name !== "silas harger") {
     console.log(`The name is ${name}`);
-    name = prompt("What is my name?");
-    if (name !== "Silas Harger") {
+    name = prompt("What is my name?").toLowerCase();
+    if (name !== "silas harger") {
       alert("Try again!");
     } else {
       alert("Correct!");
+    }
+  }
+
+  let answer = "";
+  while (answer !== "11") {
+    answer = prompt("How old am I?");
+    if (answer !== "11") {
+      alert("Try again!");
+    } else {
+      alert("Correct!");
+    }
+  }
+
+  let born = "";
+  while (born !== "atlanta georgia") {
+    born = prompt("Where was I born?")
+      .toLowerCase()
+      .replace(",", "");
+    if (born !== "atlanta georgia") {
+      alert("Try again!");
+    } else {
+      alert("Correct!");
+    }
+  }
+
+  let disease = "";
+  while (disease !== "ee") {
+    disease = prompt("What is the disease I have called?")
+      .toLowerCase()
+      .replace(".", "")
+      .replace(".", "");
+    if (disease !== "ee") {
+      alert("Try again!");
+    } else {
+      alert("Correct");
     }
   }
 }
